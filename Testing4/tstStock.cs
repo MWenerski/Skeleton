62dedbcb94ad30202b0714d09b33e2446f957c66
Maciej Wenerski <P2662047@my365.dmu.ac.uk>
2023-02-15 13:18:46 +0000

    Added InstanceOK test

---
+++ Testing4/tstStock.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Testing4
+{
+    [TestClass]
+    public class tstStock
+    {
+        [TestMethod]
+        public void InstanceOK(){
+            // creates a new instance of the class
+            clsStock AStock = new clsStock();
+            // testing to see if the class exists
+            Assert.IsNotNull(AStock);
+        }
+    }
+}
