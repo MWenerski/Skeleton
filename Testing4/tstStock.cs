98494570d37b76f97fb0166c7e30bf3ab6618d45
Maciej Wenerski <P2662047@my365.dmu.ac.uk>
2023-02-22 10:56:58 +0000

    Added Tests

--- Testing4/tstStock.cs
+++ Testing4/tstStock.cs
@@ -14,9 +14,58 @@ namespace Testing4
             // testing to see if the class exists
             Assert.IsNotNull(AStock);
         }
+
+        [TestMethod]
+        public void GameIdOK()
+        {
+            clsStock AStock = new clsStock();
+            int TestData = 0;
+            AStock.GameID = TestData;
+            Assert.AreEqual(AStock.GameID, TestData);
+
+
+        }
+        [TestMethod]
+        public void GameNameOK()
+        {
+            clsStock AStock = new clsStock();
+            String TestData = "";
+            AStock.GameName = TestData;
+            Assert.AreEqual(AStock.GameName, TestData);
+        }
+        [TestMethod]
+        public void GamePriceOK()
+        {
+            clsStock AStock = new clsStock();
+            Double TestData = 10.1;
+            AStock.GamePrice = TestData;
+            Assert.AreEqual(AStock.GamePrice, TestData);
+        }
+        [TestMethod]
+        public void AmountInStockOK()
+        {
+            clsStock AStock = new clsStock();
+            int TestData = 0;
+            AStock.AmountInStock = TestData;
+            Assert.AreEqual(AStock.AmountInStock, TestData);
+        }
+        [TestMethod]
+        public void GenreOK()
+        {
+            clsStock AStock = new clsStock();
+            String TestData = "";
+            AStock.Genre = TestData;
+            Assert.AreEqual(AStock.Genre, TestData);
+        }
+        [TestMethod]
+        public void ReleaseDateOK()
+        {
+            clsStock AStock = new clsStock();
+            //DateTime TestData = ;
+
+        }
+
     }
 
-    public class clsStock
-    {
-    }
+    
 }
