d5658aaeea1c84eb98f09a2ca673e65a3b25aee6
Cookinh000 <ojcooke333@gmail.com>
2023-02-22 10:56:56 +0000

    unit tests customer

---
+++ ClassLibrary/clsCustomer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ClassLibrary
+{
+    public class clsCustomer
+    {
+        public clsCustomer()
+        {
+        }
+
+        public bool Verified { get; set; }
+        public DateTime DateAdded { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public int CustomerID { get; set; }
+        public string Username { get; set; }
+    }
+}
