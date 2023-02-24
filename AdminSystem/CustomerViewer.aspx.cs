57c254deaa212d10f26549adc312a100902e5b1b
Cookinh000 <119118834+Cookinh000@users.noreply.github.com>
2023-02-24 15:58:06 +0000

    Update CustomerViewer.aspx.cs

--- AdminSystem/CustomerViewer.aspx.cs
+++ AdminSystem/CustomerViewer.aspx.cs
@@ -4,11 +4,17 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using ClassLibrary;
 
 public partial class _1Viewer : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-
+        //create a new instance of clsCustomer
+        clsCustomer AnCustomer = new clsCustomer();
+        //get the data from the session object
+        AnCustomer = (clsCustomer)Session["AnCustomer"];
+        //display the house number for this entry
+        Response.Write(AnCustomer.Username);
     }
 }
