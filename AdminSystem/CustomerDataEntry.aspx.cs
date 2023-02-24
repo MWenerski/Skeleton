da6a2f9d0a7fde9e48d8f22f11a4a0320bb5d92b
Cookinh000 <119118834+Cookinh000@users.noreply.github.com>
2023-02-24 15:59:14 +0000

    Update CustomerDataEntry.aspx.cs

--- AdminSystem/CustomerDataEntry.aspx.cs
+++ AdminSystem/CustomerDataEntry.aspx.cs
@@ -4,11 +4,29 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using ClassLibrary;
 
 public partial class _1_DataEntry : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
 
+    }
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void btnOK_Click(object sender, EventArgs e)
+    {
+        //create a new instance of clsCustomer
+        clsCustomer AnCustomer = new clsCustomer();
+        //capture the username
+        AnCustomer.Username = TxtUsername.Text;
+        //store the address in the session object
+        Session["AnCustomer"] = AnCustomer;
+        //nav to the viewer page
+        Response.Redirect("CustomerViewer.aspx");
     }
 }
