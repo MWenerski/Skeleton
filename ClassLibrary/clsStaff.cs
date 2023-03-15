2f43b36c806f72a6ac80c909dae5292b8e0b6a2c
P2658251 <P2658251@my365.dmu.ac.uk>
2023-03-15 10:59:40 +0000

    Find method

--- ClassLibrary/clsStaff.cs
+++ ClassLibrary/clsStaff.cs
@@ -4,11 +4,86 @@ namespace ClassLibrary
 {
     public class clsStaff
     {
-        public bool availableToWork { get; set; }
-        public DateTime dateStarted { get; set; }
-        public string phoneNumber { get; set; }
-        public double hourlyWage { get; set; }
-        public double hoursWorked { get; set; }
-        public string name { get; set; }
+        public int staffId { get; }
+        private bool mAvailableToWork;
+        public bool availableToWork
+        {
+            get
+            {
+                return mAvailableToWork;
+            }
+            set
+            {
+                mAvailableToWork = value;
+            }
+        }
+        private DateTime mDateStarted;
+        public DateTime dateStarted
+        {
+            get
+            {
+                return mDateStarted;
+            }
+            set
+            {
+                mDateStarted = value;
+            }
+        }
+        private String mPhoneNumber;
+        public string phoneNumber
+        {
+            get
+            {
+                return mPhoneNumber;
+            }
+
+            set
+            {
+                mPhoneNumber = value;
+            }
+        }
+        private double mHourlyWage;
+        public double hourlyWage
+        {
+            get
+            {
+                return mHourlyWage;
+            }
+            set
+            {
+                mHourlyWage = value;
+            }
+        }
+        private double mHoursWorked;
+        public double hoursWorked
+        {
+            get
+            {
+                return mHoursWorked;
+            }
+            set
+            {
+                mHoursWorked = value;
+            }
+        }
+        private string mName;
+        public string name
+        {
+            get
+            {
+                return mName;
+            }
+            set
+            {
+                mName = value;
+            }
+        }
+
+        public bool Find(int staffId)
+        {
+            //Dummy
+            staffId = 15;
+            return true;
+        }
     }
 }
--- Testing3/tstStaff.cs
+++ Testing3/tstStaff.cs
@@ -89,6 +89,145 @@ namespace Testing3
             Assert.AreEqual(Staff1.name, testData);
         }
 
-
+        public void FindMethodOK()
+        {
+            //Create an instance of the class we want to create
+            clsStaff aStaff = new clsStaff();
+            //Boolean variable to store results of the validation
+            Boolean found = false;
+            //Boolean variable to record if data is ok
+            Boolean OK = true;
+            //Create some test data
+            int staffId = 1;
+            //Invoke the method
+            found = aStaff.Find(staffId);
+            //Check the staff id
+            if (aStaff.staffId != 15)
+            {
+                OK = false;
+            }
+            //Test to see if the result is correct
+            Assert.IsTrue(OK);
+        }
+        public void FindPhoneNumberOK()
+        {
+            //Create an instance of the class we want to create
+            clsStaff aStaff = new clsStaff();
+            //Boolean variable to store results of the validation
+            Boolean found = false;
+            //Boolean variable to record if data is ok
+            Boolean OK = true;
+            //Create some test data
+            int staffId = 1;
+            //Invoke the method
+            found = aStaff.Find(staffId);
+            //Check the phone number
+            if (aStaff.staffId != 1)
+            {
+                OK = false;
+            }
+            //Test to see if the reuslt is correct
+            Assert.IsTrue(OK);
+        }
+        public void FindHourlyWageOK()
+        {
+            //Create an instance of the class we want to create
+            clsStaff aStaff = new clsStaff();
+            //Boolean variable to store results of the validation
+            Boolean found = false;
+            //Boolean variable to record if data is ok
+            Boolean OK = true;
+            //Create some test data
+            int staffId = 1;
+            //Invoke the method
+            found = aStaff.Find(staffId);
+            //Check the hourly wage
+            if (aStaff.hourlyWage != 10.50)
+            {
+                OK = false;
+            }
+            //Test to see if the result is correct
+            Assert.IsTrue(OK);
+        }
+        public void FindHoursWorkedOK()
+        {
+            //Create an instance of the class we want to create
+            clsStaff aStaff = new clsStaff();
+            //Boolean variable to store results of the validation
+            Boolean found = false;
+            //Boolean variable to record if data is ok
+            Boolean OK = true;
+            //Create some test data
+            int staffId = 1;
+            //Invoke the method
+            found = aStaff.Find(staffId);
+            //Check the hours worked
+            if (aStaff.hoursWorked != 9.50)
+            {
+                OK = false;
+            }
+            //Test to see if the result is correct
+            Assert.IsTrue(OK);
+        }
+        public void FindNameOK()
+        {
+            //Create an instance of the class we want to create
+            clsStaff aStaff = new clsStaff();
+            //Boolean variable to store results of the validation
+            Boolean found = false;
+            //Boolean variable to record if data is ok
+            Boolean OK = true;
+            //Create some test data
+            int staffId = 1;
+            //Invoke the method
+            found = aStaff.Find(staffId);
+            //Check the name
+            if (aStaff.name != "John Smith")
+            {
+                OK = false;
+            }
+            //Test to see if the result is correct
+            Assert.IsTrue(OK);
+        }
+        public void FindAvailableToWorkOK()
+        {
+            //Create an instance of the class we want to create
+            clsStaff aStaff = new clsStaff();
+            //Boolean variable to store results of the validation
+            Boolean found = false;
+            //Boolean variable to record if data is ok
+            Boolean OK = true;
+            //Create some test data
+            int staffId = 1;
+            //Invoke the method
+            found = aStaff.Find(staffId);
+            //Check available to work
+            if (aStaff.availableToWork != true)
+            {
+                OK = false;
+            }
+            //Test to see if the reuslt is correct
+            Assert.IsTrue(OK);
+        }
+        public void FindDateStartedOK()
+        {
+            //Create an instance of the class we want to create
+            clsStaff aStaff = new clsStaff();
+            //Boolean variable to store results of the validation
+            Boolean found = false;
+            //Boolean variable to record if data is ok
+            Boolean OK = true;
+            //Create some test data
+            int staffId = 1;
+            //Invoke the method
+            found = aStaff.Find(staffId);
+            //Check the date started
+            if (aStaff.dateStarted != Convert.ToDateTime("16/09/2015"))
+            {
+                OK = false;
+            }
+            //Test to see if the reuslt is correct
+            Assert.IsTrue(OK);
+        }
     }
 }
