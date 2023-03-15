4357c489d967895809918b15b170870729d4dc07
P2658251 <P2658251@my365.dmu.ac.uk>
2023-03-15 01:08:22 +0000

    Work up to enhancement week

--- ClassLibrary/clsStaff.cs
+++ ClassLibrary/clsStaff.cs
@@ -1,6 +1,14 @@
-﻿namespace ClassLibrary
+﻿using System;
+
+namespace ClassLibrary
 {
     public class clsStaff
     {
+        public bool availableToWork { get; set; }
+        public DateTime dateStarted { get; set; }
+        public string phoneNumber { get; set; }
+        public double hourlyWage { get; set; }
+        public double hoursWorked { get; set; }
+        public string name { get; set; }
     }
 }
