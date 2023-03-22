d3e9b7c8680faa7d27da69c94d405b2b5ddcce45
MedBdft <P2704563@my365.dmu.ac.uk>
2023-03-22 01:46:12 +0000

    added the inital implementation of the find method

--- ClassLibrary/clsSupplier.cs
+++ ClassLibrary/clsSupplier.cs
@@ -4,12 +4,32 @@ namespace ClassLibrary
 {
     public class clsSupplier
     {
+        //private data memeber for the ID property
+        private int mID;
+        public int ID
+        {
+            get
+            {
+                //this line of code sends data out the property
+                return mID;
+            }
+            set
+            {
+                //this line of code allows data into the property
+                mID = value;
+            }
+        }
         public string SupplierName { get; set; }
-        public int ID { get; set; }
         public string Email { get; set; }
         public DateTime DateAdded { get; set; }
-        public int ContactNumber { get; set; }
+        public string ContactNumber { get; set; }
         public string Address { get; set; }
         public bool OngoingContract { get; set; }
+
+        public bool Find(int iD)
+        {
+            mID = 2;
+            return true;
+        }
     }
 }
